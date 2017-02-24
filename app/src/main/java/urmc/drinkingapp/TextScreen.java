0fd7717275cc32b87ac491654fa61f32e2895191
rreynos0 <rreynoso@u.rochester.edu>
2017-02-23 20:00:32 -0500

    test

--- app/src/main/java/urmc/drinkingapp/TextScreen.java
+++ app/src/main/java/urmc/drinkingapp/TextScreen.java
@@ -9,5 +9,10 @@ public class TextScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_screen);
+
+
+
+
+        //HIIII
     }
 }
