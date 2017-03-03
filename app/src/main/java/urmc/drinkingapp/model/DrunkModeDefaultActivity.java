ce93b8696198d5a13a77fc9bf0d3280b21acf45b
rreynos0 <rreynoso@u.rochester.edu>
2017-03-02 21:02:15 -0500

    test

---
+++ app/src/main/java/urmc/drinkingapp/model/DrunkModeDefaultActivity.java
@@ -0,0 +1,19 @@
+package urmc.drinkingapp.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import urmc.drinkingapp.R;
+
+public class
+DrunkModeDefaultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drunk_mode_default);
+    }
+
+
+
+}
--- app/src/main/java/urmc/drinkingapp/model/DrunkModeWithFriend.java
+++ app/src/main/java/urmc/drinkingapp/model/DrunkModeWithFriend.java
@@ -1,18 +1,17 @@
-package urmc.drinkingapp;
+package urmc.drinkingapp.model;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TextScreen extends AppCompatActivity {
+import urmc.drinkingapp.R;
+
+public class DrunkModeWithFriend extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text_screen);
-
-
-
-
-        //HIIII
+        setContentView(R.layout.activity_drunk_mode_with_friend);
     }
+
 }
+
